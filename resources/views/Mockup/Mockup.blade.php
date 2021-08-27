859b02a179c95c48e57ed2e4d0a3726c395338fe
nguyentrungtin913 <82808672+nguyentrungtin913@users.noreply.github.com>
2021-08-27 11:18:18 +0700

    27/08

--- resources/views/Mockup/Mockup.blade.php
+++ resources/views/Mockup/Mockup.blade.php
@@ -8,11 +8,11 @@
 <body>
    <a href="{{URL::to('/create-mockup')}}">Add mockup</a>
     <div style="width:98%;margin: auto;text-align: center;">
-         
+
       @foreach($mockups as $key => $mockup)
         <div style="border: solid thin gray;float: left; margin:3% 0 0 3%;">
             <a href="{{URL::to('/show-mockup/'.$mockup->mockup_id)}}">
-                <img src='{{URL::to("storage/app/public/mockup/".$mockup->mockupType["type_name"]."/".$mockup->mockup_name."?nocache=".time())}}' height="170" width="220">
+                <img src='{{URL::to("storage/Images/mockup/".$mockup->mockupType["type_name"]."/".$mockup->mockup_name."?nocache=".time())}}' height="170" width="220">
             </a>
             <p>{{number_format($mockup->mockup_price,0)}} đ</p>
             <div style="width: 100%;padding: auto;margin: 1%;text-align: center;" >
@@ -23,6 +23,6 @@
         </div>
         @endforeach
     </div>
-    
+
 </body>
 </html>
