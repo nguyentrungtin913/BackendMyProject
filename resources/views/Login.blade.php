bfc6c7fbc713e1886cdb7c4d5558c91fd878b684
nguyentrungtin913 <82808672+nguyentrungtin913@users.noreply.github.com>
2021-08-31 09:08:55 +0700

    31/08

--- resources/views/Login.blade.php
+++ resources/views/Login.blade.php
@@ -16,7 +16,7 @@
 	  </div>
 	  <div class="mb-3">
 	    <label for="exampleInputPassword1" class="form-label">Password</label>
-	    <input type="password" class="form-control" name = "password">
+	    <input type="password" class="form-control" name = "password" value="qwe">
 	  </div>
 	  <div class="mb-3 form-check">
 	    <input type="checkbox" class="form-check-input" id="exampleCheck1">
